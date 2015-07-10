72c5cedce3371b0ff008edb6cf9849dad58b5262
Dave Vernon <dave.vernon@southbankcentre.co.uk>
2015-07-10 14:59:49 +0100

    add test scope data

--- app/southbankcentre.run.js
+++ app/southbankcentre.run.js
@@ -4,5 +4,9 @@ angular
   .module('southbankcentre')
   .run(function () {
 
+  })
+  .controller('TestCtrl', function($scope){
+      var test = this;
+      test = { name: 'Hello world' };
+      $scope.test = test;
   });
-
