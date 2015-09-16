ea429128d1a4172e3ac7950ed90becf997fa53fe
samueljmurray <samueljmurray@gmail.com>
2015-09-16 16:31:44 +0100

    add scArrangement override app

---
+++ app/components/southbankcentreArrangement/southbankcentreArrangement.module.js
@@ -0,0 +1,15 @@
+(function() {
+  'use strict';
+
+  /**
+   * @ngdoc overview
+   * @name southbankcentre
+   * @description
+   *
+   * !! Module not actually needed as it only provides a view template at this point
+   */
+  angular
+    .module('southbankcentreArrangement', [
+      'southbankcentre'
+    ]);
+})();
