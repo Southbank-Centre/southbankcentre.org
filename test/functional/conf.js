462b30c6b1b33e630e340a542a21e14927209c0d
samueljmurray <samueljmurray@gmail.com>
2015-09-28 14:07:40 +0100

    functional tests for single event page

--- test/functional/conf.js
+++ test/functional/conf.js
@@ -11,13 +11,86 @@
 
 // Determines whether or not to run the tests on sauce labs
 function useSauceLabs() {
-  var sauceLabs = exports.config.params.isSauceLabs;
+  var sauceLabs = 0;
   var sauceLabsPos = process.argv.indexOf('--params.isSauceLabs');
   if (sauceLabsPos > -1) {
     sauceLabs = parseInt(process.argv[sauceLabsPos + 1]);
   }
 
   return sauceLabs;
+}
+
+// Determines which capabilities to test
+// Test minimal capabilities locally
+// Test full suite on Sauce Labs
+function determineCapabilities() {
+  var capabilities;
+  if (useSauceLabs()) {
+    capabilities = [
+      {
+        browserName: 'chrome',
+        name: 'Testing with chrome',
+        'tunnel-identifier': process.env.SAUCE_TUNNEL_ID
+      },
+      {
+        browserName: 'firefox',
+        name: 'Testing with firefox',
+        'tunnel-identifier': process.env.SAUCE_TUNNEL_ID
+      },
+      {
+        browserName: 'internet explorer',
+        name: 'Testing with IE9',
+        version: '9',
+        'tunnel-identifier': process.env.SAUCE_TUNNEL_ID
+      },
+      {
+        browserName: 'internet explorer',
+        name: 'Testing with IE10',
+        version: '10',
+        'tunnel-identifier': process.env.SAUCE_TUNNEL_ID
+      },
+      {
+        browserName: 'internet explorer',
+        name: 'Testing with IE11',
+        version: '11',
+        'tunnel-identifier': process.env.SAUCE_TUNNEL_ID
+      },
+      {
+        browserName: 'safari',
+        name: 'Testing with Safari 8',
+        version: '8',
+        'tunnel-identifier': process.env.SAUCE_TUNNEL_ID
+      },
+      {
+        platformName: 'iOS',
+        platformVersion: '8.4',
+        browserName: '',
+        app: 'safari',
+        deviceName: 'iPhone Simulator',
+        name: 'Testing with iOS 8.4 Safari',
+        'appium-version': '1.4.0',
+        'tunnel-identifier': process.env.SAUCE_TUNNEL_ID
+      },
+      {
+        platformName: 'Android',
+        platformVersion: '4.4',
+        browserName: 'Browser',
+        deviceName: 'Android Emulator',
+        name: 'Testing with Android 4.4 browser',
+        'appium-version': "1.4.0",
+        'tunnel-identifier': process.env.SAUCE_TUNNEL_ID
+      }
+    ];
+  } else {
+    capabilities = [
+      {
+        browserName: 'chrome',
+        name: 'Testing with chrome',
+        'tunnel-identifier': process.env.SAUCE_TUNNEL_ID
+      }
+    ];
+  }
+  return capabilities;
 }
 
 exports.config = {
@@ -51,65 +124,18 @@ exports.config = {
   // ]
 
   // Multiple Browsers
-  multiCapabilities: [
-    {
-      browserName: 'chrome',
-      name: 'Testing with chrome',
-      'tunnel-identifier': process.env.SAUCE_TUNNEL_ID
-    },
-    {
-      browserName: 'firefox',
-      name: 'Testing with firefox',
-      'tunnel-identifier': process.env.SAUCE_TUNNEL_ID
-    },
-    {
-      browserName: 'internet explorer',
-      name: 'Testing with IE9',
-      version: '9',
-      'tunnel-identifier': process.env.SAUCE_TUNNEL_ID
-    },
-    {
-      browserName: 'internet explorer',
-      name: 'Testing with IE10',
-      version: '10',
-      'tunnel-identifier': process.env.SAUCE_TUNNEL_ID
-    },
-    {
-      browserName: 'internet explorer',
-      name: 'Testing with IE11',
-      version: '11',
-      'tunnel-identifier': process.env.SAUCE_TUNNEL_ID
-    },
-    {
-      browserName: 'safari',
-      name: 'Testing with Safari 8',
-      version: '8',
-      'tunnel-identifier': process.env.SAUCE_TUNNEL_ID
-    },
-    {
-      platformName: 'iOS',
-      platformVersion: '8.4',
-      browserName: '',
-      app: 'safari',
-      deviceName: 'iPhone Simulator',
-      name: 'Testing with iOS 8.4 Safari',
-      'appium-version': '1.4.0',
-      'tunnel-identifier': process.env.SAUCE_TUNNEL_ID
-    },
-    {
-      platformName: 'Android',
-      platformVersion: '4.4',
-      browserName: 'Browser',
-      deviceName: 'Android Emulator',
-      name: 'Testing with Android 4.4 browser',
-      'appium-version': "1.4.0",
-      'tunnel-identifier': process.env.SAUCE_TUNNEL_ID
-    }
-  ],
+  multiCapabilities: determineCapabilities(),
 
   params: {
-    url: 'http://sc.circle:8080'
+    url: 'http://sc.circle:8080',
+    isSauceLabs: 0
   },
+
+  allScriptsTimeout: 31000,
+
+  getPageTimeout: 30000,
+
+  rootElement: 'html',
 
   framework: 'jasmine2',
 
@@ -118,10 +144,10 @@ exports.config = {
     // If true, print colors to the terminal.
     showColors: true,
     // Default time to wait in ms before a test fails.
-    defaultTimeoutInterval: 30000
+    defaultTimeoutInterval: 120000
   },
 
-  onPrepare: function(){
+  onPrepare: function() {
     // Syntactic sugar to set whether we're testing an angular site or not
     global.isAngularSite = function(flag){
       browser.ignoreSynchronization = !flag;
---
+++ test/functional/specs/eventSingle/eventSingle-spec.js
@@ -0,0 +1,111 @@
+'use strict';
+/**
+ * eventSingle Spec
+ */
+
+describe('The single event page', function() {
+
+  beforeEach(function() {
+    browser.addMockModule('mock.scContent', function() {
+      angular.module('mock.scContent', [])
+        .factory('scContentGet', function($q) {
+            var testEvents = {
+              'single-day-single-perf-123': {
+                'name': 'Event name',
+                'field_event_prefix': 'Event prefix',
+                'field_event_suffix': 'Event suffix',
+                'startDate': '2016-02-23T00:00:00+00:00',
+                'endDate': '2016-02-23T00:00:00+00:00',
+                'subEvent': [
+                  'http://example.com/api/event/performance-name-123.json'
+                ]
+              },
+              'multi-day-single-perf-123': {
+                'name': 'Event name',
+                'field_event_prefix': 'Event prefix',
+                'field_event_suffix': 'Event suffix',
+                'startDate': '2016-02-23T00:00:00+00:00',
+                'endDate': '2016-02-25T00:00:00+00:00',
+                'subEvent': [
+                  'http://example.com/api/event/performance-name-123.json'
+                ]
+              },
+              'single-day-multi-perf-123': {
+                'name': 'Event name',
+                'field_event_prefix': 'Event prefix',
+                'field_event_suffix': 'Event suffix',
+                'startDate': '2016-02-23T00:00:00+00:00',
+                'endDate': '2016-02-23T00:00:00+00:00',
+                'subEvent': [
+                  'http://example.com/api/event/performance-name-123.json',
+                  'http://example.com/api/event/performance-name-123.json'
+                ]
+              },
+              'multi-day-multi-perf-123': {
+                'name': 'Event name',
+                'field_event_prefix': 'Event prefix',
+                'field_event_suffix': 'Event suffix',
+                'startDate': '2016-02-23T00:00:00+00:00',
+                'endDate': '2016-02-25T00:00:00+00:00',
+                'subEvent': [
+                  'http://example.com/api/event/performance-name-123.json',
+                  'http://example.com/api/event/performance-name-123.json',
+                  'http://example.com/api/event/performance-name-123.json'
+                ]
+              }
+            };
+
+            var testPerformance = {
+              'name': 'Event name',
+              'startDate': '2016-02-23T19:30:11+00:00',
+              'endDate': '2016-02-25T21:52:12+00:00'
+            };
+
+            return {
+
+              item: function(type, id, options) { 
+                return $q.when(testEvents[id]);
+              },
+
+              itemByPath: function() {
+                return $q.when(testPerformance);
+              }
+
+            };
+        });
+    });
+  });
+
+  it('should display a single-day event with a single performance correctly', function() {
+    browser.get(browser.params.url + '#!event/single-day-single-perf-123');
+    expect(element(by.css('.event-title__prefix')).getText()).toEqual('Event prefix');
+    expect(element(by.css('.event-title__name')).getText()).toEqual('Event name');
+    expect(element(by.css('.event-title__suffix')).getText()).toEqual('Event suffix');
+    expect(element(by.css('.event-date')).getText()).toEqual('Tuesday 23 February 2016 at 7:30pm');
+  });
+
+  it('should display a multi-day event with a single performance correctly', function() {
+    browser.get(browser.params.url + '#!event/multi-day-single-perf-123');
+    expect(element(by.css('.event-title__prefix')).getText()).toEqual('Event prefix');
+    expect(element(by.css('.event-title__name')).getText()).toEqual('Event name');
+    expect(element(by.css('.event-title__suffix')).getText()).toEqual('Event suffix');
+    expect(element(by.css('.event-date')).getText()).toEqual('Tuesday 23 February 2016 – Thursday 25 February 2016 at 7:30pm');
+  });
+
+  it('should display a single-day event with a multiple performances correctly', function() {
+    browser.get(browser.params.url + '#!event/single-day-multi-perf-123');
+    expect(element(by.css('.event-title__prefix')).getText()).toEqual('Event prefix');
+    expect(element(by.css('.event-title__name')).getText()).toEqual('Event name');
+    expect(element(by.css('.event-title__suffix')).getText()).toEqual('Event suffix');
+    expect(element(by.css('.event-date')).getText()).toEqual('Tuesday 23 February 2016');
+  });
+
+  it('should display a multi-day event with a multiple performances correctly', function() {
+    browser.get(browser.params.url + '#!event/multi-day-multi-perf-123');
+    expect(element(by.css('.event-title__prefix')).getText()).toEqual('Event prefix');
+    expect(element(by.css('.event-title__name')).getText()).toEqual('Event name');
+    expect(element(by.css('.event-title__suffix')).getText()).toEqual('Event suffix');
+    expect(element(by.css('.event-date')).getText()).toEqual('Tuesday 23 February 2016 – Thursday 25 February 2016');
+  });
+
+});
