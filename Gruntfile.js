d96c370617fd6c1e8aee669bec06872938049092
samueljmurray <samueljmurray@gmail.com>
2015-09-28 14:07:07 +0100

    changes to support testing in IE9 and Safari

--- Gruntfile.js
+++ Gruntfile.js
@@ -106,11 +106,16 @@ module.exports = function (grunt) {
     // The actual grunt server settings
     connect: {
       options: {
-        port: 9002,
+        port: 9001,
         // Change this to '0.0.0.0' to access the server from outside.
         // hostname: 'localhost',
         hostname: 'localhost',
-        livereload: 35729
+        livereload: 35729,
+        middleware: function () {
+          return [
+            modRewrite (['^ /index.html']),
+          ];
+        }
       },
       proxies: [
         {
