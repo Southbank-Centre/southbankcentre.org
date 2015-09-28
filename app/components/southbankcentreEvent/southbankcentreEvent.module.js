f2a11fe13052df7077f9f4d20c00ef36ccc9569c
samueljmurray <samueljmurray@gmail.com>
2015-09-28 14:10:47 +0100

    create southbankcentreEvent module with a single event view and controller

---
+++ app/components/southbankcentreEvent/southbankcentreEvent.module.js
@@ -0,0 +1,16 @@
+(function() {
+  'use strict';
+
+  /**
+   * @ngdoc overview
+   * @name southbankcentreEvent
+   * @description
+   *
+   * Provides logic for managing and exposing Southbank Centre Event content
+   */
+  angular
+    .module('southbankcentreEvent', [
+      'scContent',
+      'ui.router'
+    ]);
+})();
