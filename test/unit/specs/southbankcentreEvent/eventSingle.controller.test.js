cb8185ffc2738cbc15c86bbddfbd9e9bafa051e0
Sam Murray <samueljmurray@gmail.com>
2015-09-29 15:37:26 +0100

    tests for 404 page display if event not found

--- test/unit/specs/southbankcentreEvent/eventSingle.controller.test.js
+++ test/unit/specs/southbankcentreEvent/eventSingle.controller.test.js
@@ -5,14 +5,18 @@
   angular.module('mock.scContent', [])
     .factory('scContentGet', function($q) {
         return {
-          item: function() {
-            return $q.when({
-              'event':'here',
-              'subEvent':[
-                'http://example.com/api/performance/123.json',
-                'http://example.com/api/performance/124.json'
-              ]
-            });
+          item: function(type, id, options) {
+            var testData;
+            if (id === 'exists') {
+              testData = {
+                'event':'here',
+                'subEvent':[
+                  'http://example.com/api/performance/123.json',
+                  'http://example.com/api/performance/124.json'
+                ]
+              };
+            }
+            return $q.when(testData);
           },
           itemByPath: function() {
             return $q.when({
@@ -25,29 +29,36 @@
   describe('Controller: EventSingleCtrl', function() {
 
     var $controller;
-    var $stateParams;
     var $timeout;
+    var $state;
+    var $stateParams;
     var EventSingleCtrl;
     var scContentGet;
 
     beforeEach(module('ui.router'));
     beforeEach(module('southbankcentreEvent'));
     beforeEach(module('mock.scContent'));
+    beforeEach(module(function ($stateProvider) {
+      $stateProvider.state('app', {});
+      $stateProvider.state('404', {});
+    }));
 
-    beforeEach(inject(function(_$controller_, _scContentGet_, _$stateParams_, _$timeout_, $q) {
+    beforeEach(inject(function(_$controller_, _scContentGet_, _$timeout_, _$state_, _$stateParams_, $q) {
       $controller = _$controller_;
-      $stateParams = _$stateParams_;
       $timeout = _$timeout_;
+      $state = _$state_;
+      $stateParams = _$stateParams_;
       scContentGet = _scContentGet_;
-
-      EventSingleCtrl = $controller('EventSingleCtrl', {});
     }));
 
     it('should expose an event property', function() {
+      EventSingleCtrl = $controller('EventSingleCtrl', {});
       expect(EventSingleCtrl.event).toBeDefined();
     });
 
     it('should get the Event upon activation, loading in each performance', function() {
+      $stateParams.id = "exists";
+      EventSingleCtrl = $controller('EventSingleCtrl', {});
       expect(EventSingleCtrl.event).toEqual({});
       $timeout.flush();
       expect(EventSingleCtrl.event).toEqual({
@@ -61,6 +72,17 @@
           {'performance':'here'}
         ]
       });
+    });
+
+    it('should display a 404 page if the event does not exist', function() {
+      $stateParams.id = "does-not-exist";
+      EventSingleCtrl = $controller('EventSingleCtrl', {});
+      expect(EventSingleCtrl.event).toEqual({});
+
+      $timeout.flush();
+
+      expect(EventSingleCtrl.event).toBe(undefined);
+      expect($state.current.name).toEqual('404');
     });
 
     describe('Method: getEvent', function() {
